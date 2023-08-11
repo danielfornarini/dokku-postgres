561f1c5bbd98ddad9629aebad1278147c87f9c82
Daniel Fornarini <daniel@monade.io>
2023-08-11 11:40:07 +0200

    edd echo for tests

--- scripts/create_ssl_certs.sh
+++ scripts/create_ssl_certs.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 
-source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/config"
-set -eo pipefail
-[[ $DOKKU_TRACE ]] && set -x
-source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/common-functions"
-source "$PLUGIN_CORE_AVAILABLE_PATH/common/functions"
-source "$PLUGIN_CORE_AVAILABLE_PATH/common/property-functions"
-source "$PLUGIN_AVAILABLE_PATH/config/functions"
-if [[ -f "$PLUGIN_AVAILABLE_PATH/docker-options/functions" ]]; then
-  source "$PLUGIN_AVAILABLE_PATH/docker-options/functions"
-fi
-
 postgres_service_dir="$1"
-
-dokku_log_verbose_quiet "ciao"
-dokku_log_verbose_quiet "$postgres_service_dir"
 
 cd "$postgres_service_dir"
 mkdir certs && cd certs
